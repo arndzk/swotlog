6a59ba1083a821a79778517a80e8e5996fe784ed
Michael Garganourakis <garganourakis.m@gmail.com>
2019-11-17 17:50:45 +0200

    initialize views

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ module.exports = {
   webpack(config) {
     config.resolve.alias['components'] = path.resolve(__dirname, 'components/');
     config.resolve.alias['constants'] = path.resolve(__dirname, 'constants/');
+    config.resolve.alias['views'] = path.resolve(__dirname, 'views/');
     return config;
   },
 };
--- pages/index.js
+++ pages/index.js
@@ -1,77 +1,8 @@
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import Container from "@material-ui/core/Container";
-import Button from "@material-ui/core/Button";
-import Link from "components/Link";
-import { HOME_CARDS } from 'constants/texts';
+import HomeGuest from 'views/HomeGuest';
 
-import useStyles from "./styles";
+const Home = () => {
 
-export default function Album() {
-  const classes = useStyles();
-
-  return (
-    <>
-      <div className={classes.heroContent}>
-        <Container maxWidth="sm">
-          <Typography
-            component="h1"
-            variant="h2"
-            align="center"
-            color="textPrimary"
-            gutterBottom
-          >
-            Swotlog
-          </Typography>
-          <Typography
-            variant="h5"
-            align="center"
-            color="textSecondary"
-            paragraph
-          >
-            A service designed to unite students. Become a member of our
-            community and make your studies a walk in the park
-          </Typography>
-          <div className={classes.heroButtons}>
-            <Grid container spacing={2} justify="center">
-              <Grid item>
-                <Link href="/signup">
-                  <Button variant="contained" color="primary">
-                    Become a member
-                  </Button>
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="/signin">
-                  <Button variant="outlined" color="primary">
-                    Sing in
-                  </Button>
-                </Link>
-              </Grid>
-            </Grid>
-          </div>
-        </Container>
-      </div>
-      <Container className={classes.cardGrid} maxWidth="md">
-        <Grid container spacing={4}>
-          {HOME_CARDS.map(card => (
-            <Grid item key={card.heading} xs={12} sm={6} md={4}>
-              <Card className={classes.card}>
-                <CardContent className={classes.cardContent}>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    {card.heading}
-                  </Typography>
-                  <Typography>
-                    {card.content}
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
-      </Container>
-    </>
-  );
+  return <HomeGuest />  
 }
+
+export default Home;
