d02290b89ebbfd562ab3ee5328acf7f88ccb8356
Michael Garganourakis <garganourakis.m@gmail.com>
2019-11-17 13:35:55 +0200

    init footer

---
+++ components/Footer/index.js
@@ -0,0 +1,12 @@
+export default () => {
+  return <Box mt={8}>
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  </Box>
+}
