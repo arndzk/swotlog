b2c897f33f6074698519b17758df1d59ae25a7df
Michael Garganourakis <garganourakis.m@gmail.com>
2019-11-17 13:38:25 +0200

    init _app.js: - ThemeProvider - Theme sample - CssBaseline

---
+++ pages/_app.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import NextApp from 'next/app';
+import Head from 'next/head';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import { ThemeProvider } from '@material-ui/core/styles';
+
+import theme from '../utils/theme';
+
+export default class App extends NextApp {
+  // Only uncomment this method if you have blocking data requirements for
+  // every single page in your application. This disables the ability to
+  // perform automatic static optimization, causing every page in your app to
+  // be server-side rendered.
+  //
+  // static async getInitialProps(appContext) {
+  //   // calls page's `getInitialProps` and fills `appProps.pageProps`
+  //   const appProps = await App.getInitialProps(appContext);
+  //
+  //   return { ...appProps }
+  // }
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <React.Fragment>
+        <Head>
+          <title>Swotlog</title>
+        </Head>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Container component="main" maxWidth="xs">
+            <Component {...pageProps} />
+          </Container>
+        </ThemeProvider>
+      </React.Fragment>
+    );
+  }
+}
