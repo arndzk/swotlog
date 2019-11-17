6717fa745c9cc55794841009771c9cc1c098c7b1
Michael Garganourakis <garganourakis.m@gmail.com>
2019-11-17 16:55:12 +0200

    dummy homepage

---
+++ pages/styles.js
@@ -0,0 +1,23 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles(theme => ({
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6)
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4)
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8)
+  },
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column"
+  },
+  cardContent: {
+    flexGrow: 1
+  }
+}));
