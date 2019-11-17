811e7878c41be054e441bfa0c7e95bcb9375f3a7
Michael Garganourakis <garganourakis.m@gmail.com>
2019-11-17 16:54:24 +0200

    _app.js changes: - Dynamic page title based on route - Footer & Header everywhere - Dynamic container size based on route

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import NextApp from 'next/app';
-import Head from 'next/head';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Container from '@material-ui/core/Container';
-import { ThemeProvider } from '@material-ui/core/styles';
+import NextApp from "next/app";
+import Head from "next/head";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Container from "@material-ui/core/Container";
+import { ThemeProvider } from "@material-ui/core/styles";
+import Footer from "components/Footer";
+import Header from "components/Header";
+import { PAGE_TITLES } from "constants/misc";
 
-import theme from '../utils/theme';
-
+import theme from "../utils/theme";
 export default class App extends NextApp {
   // Only uncomment this method if you have blocking data requirements for
   // every single page in your application. This disables the ability to
@@ -21,27 +22,38 @@ export default class App extends NextApp {
   // }
   componentDidMount() {
     // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
+    const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
   }
 
   render() {
-    const { Component, pageProps } = this.props;
+    const {
+      Component,
+      pageProps,
+      router: { asPath }
+    } = this.props;
+    const [, main, sub] = asPath.split("/");
+    const title = PAGE_TITLES[main];
 
     return (
-      <React.Fragment>
+      <>
         <Head>
-          <title>Swotlog</title>
+          <title>{title ? `${title} | ` : ""}Swotlog</title>
         </Head>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-          <Container component="main" maxWidth="xs">
+          <Header />
+          <Container
+            component="main"
+            {...(["signin", "signup"].includes(main) && { maxWidth: "xs" })}
+          >
             <Component {...pageProps} />
           </Container>
+          <Footer />
         </ThemeProvider>
-      </React.Fragment>
+      </>
     );
   }
 }
