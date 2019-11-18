4f9bb41ee2bb127eda3f4b2f82d3747114ad2c98
Michael Garganourakis <garganourakis.m@gmail.com>
2019-11-18 23:34:54 +0200

    user bubble

---
+++ components/Bubble/index.js
@@ -0,0 +1,68 @@
+import Router from 'next/router'
+import { connect } from 'react-redux';
+import IconButton from '@material-ui/core/IconButton';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import { logout } from 'actions/user';
+
+const Bubble = ({ user, logout }) => {
+  if (!user.id) return null;
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const actions = { // may have more
+    logout,
+  }
+
+  const handleChange = event => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = ({ route, action }) => {
+    if (route) 
+      Router.push(route)
+
+    if (action) {
+      actions[action]();
+    }
+
+    setAnchorEl(null);
+  };
+
+  return  <div>
+    <IconButton
+      aria-label="account of current user"
+      aria-controls="menu-appbar"
+      aria-haspopup="true"
+      onClick={handleMenu}
+      color="inherit"
+    >
+      <AccountCircle />
+    </IconButton>
+    <Menu
+      id="menu-appbar"
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={open}
+      onClose={handleClose}
+    >
+      <MenuItem onClick={() => handleClose({ route: '/profile'})}>Profile</MenuItem>
+      <MenuItem onClick={() => handleClose({ action: 'logout' })}>Logout</MenuItem>
+    </Menu>
+  </div>
+}
+
+export default connect(state => ({ user: state.user }), { logout })(Bubble);
