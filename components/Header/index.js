72102d61882b14042d492bee0535241be1438237
Michael Garganourakis <garganourakis.m@gmail.com>
2019-11-18 23:33:14 +0200

    header styles, user bubble

--- components/Header/index.js
+++ components/Header/index.js
@@ -3,17 +3,21 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import SchoolIcon from '@material-ui/icons/School';
 import Link from 'components/Link';
+import Bubble from 'components/Bubble';
 
 import useStyles from './styles';
 
 export default () => {
   const classes = useStyles();
 
-  return <AppBar position="relative">
-    <Toolbar>
-      <Typography variant="h6" color="inherit" noWrap>
-        <Link className={classes.logoLink} color="inherit" href="/">Swotlog <SchoolIcon /></Link>
-      </Typography>
-    </Toolbar>
-  </AppBar>
+  return <div className={classes.root}>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography className={classes.title} variant="h6" color="inherit" noWrap>
+          <Link className={classes.logoLink} color="inherit" href="/">Swotlog <SchoolIcon /></Link>
+        </Typography>
+        <Bubble />
+      </Toolbar>
+    </AppBar>
+  </div>
 }
--- components/Header/styles.js
+++ components/Header/styles.js
@@ -1,9 +1,15 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(() => ({
+  root: {
+    flexGrow: 1,
+  },
   logoLink: {
     "&:hover": {
       textDecoration: 'none'
-    }
+    },
+  },
+  title: {
+    flexGrow: 1
   }
 }));
