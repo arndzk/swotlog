20626278d9df83451f7c3c979f7713bb34fa1149
Michael Garganourakis <garganourakis.m@gmail.com>
2019-11-19 01:25:57 +0200

    styles minor

--- views/HomeGuest/styles.js
+++ views/HomeGuest/styles.js
@@ -2,7 +2,6 @@ import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles(theme => ({
   heroContent: {
-    backgroundColor: theme.palette.background.paper,
     padding: theme.spacing(8, 0, 6)
   },
   heroButtons: {
