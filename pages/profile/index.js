6af4463ba28ae099b9786d41a0ffddde3bec0ec2
Michael Garganourakis <garganourakis.m@gmail.com>
2019-11-18 23:34:01 +0200

    profile route init

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,8 +1,20 @@
+import { connect } from 'react-redux';
+import Grid from '@material-ui/core/Grid';
+import AccountCircle from '@material-ui/icons/AccountCircle';
 
-const Profile = props => {
+import useStyles from './styles';
+
+const Profile = ({ user }) => {
+  const classes = useStyles();
+
   return (
-      <div>Hello!</div>
+    <Grid 
+      className={classes.wrapper} 
+      container justify="center" 
+      alignItems="center">
+        <AccountCircle className={classes.avatar} />
+    </Grid>
   );
 };
 
-export default Profile;
+export default connect(state => ({ user: state.user }))(Profile);
---
+++ pages/profile/styles.js
@@ -0,0 +1,11 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(theme => ({
+  avatar: {
+    width: 128,
+    height: 128
+  },
+  wrapper: {
+    marginTop: theme.spacing(4),
+  }
+}));
