51ecadc6c3dd762fe825af661f55005ce09f6416
Michael Garganourakis <garganourakis.m@gmail.com>
2019-11-19 01:25:46 +0200

    feed sample card

--- views/Feed/index.js
+++ views/Feed/index.js
@@ -1,5 +1,46 @@
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import CommentIcon from '@material-ui/icons/Comment';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import MenuBookIcon from '@material-ui/icons/MenuBook';
+import GroupIcon from '@material-ui/icons/Group';
+
+import useStyles from './styles';
+
 const Feed = () => {
-  return <div>1</div>
+  const classes = useStyles();
+  const domain = 'class'; // api will return either class or group
+
+  const icon = {
+    class: <MenuBookIcon className={classes.domainIcon} />,
+    group: <GroupIcon className={classes.domainIcon} />
+  }
+  return <div className={classes.feed}>
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography className={classes.domain} color="textSecondary" gutterBottom>
+          Artificial Intelligence {icon[domain]}
+        </Typography>
+        <Typography className={classes.userInfo} component="b">
+          <AccountCircleIcon className={classes.avatar} /> Takis Takopoulopoulakopoulos
+        </Typography>
+        <Typography className={classes.content} variant="body2" component="p">
+          Anyone able to solve homework 2.3? I really need some help.
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <ButtonGroup fullWidth>
+          <Button startIcon={<ThumbUpIcon color="primary" />}>Like</Button>
+          <Button startIcon={<CommentIcon color="primary" />}>Comment</Button>
+        </ButtonGroup>
+      </CardActions>
+    </Card>
+  </div>
 }
 
 export default Feed;
--- views/Feed/styles.js
+++ views/Feed/styles.js
@@ -1,3 +1,29 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-export default makeStyles(theme => ({}));
+export default makeStyles(theme => ({
+  feed: {
+    padding: theme.spacing(8, 0, 6)
+  },
+  card: {
+    minWidth: 275,
+  },
+  domain: {
+    fontSize: 14,
+  },
+  domainIcon: {
+    fontSize: 13
+  },
+  avatar: {
+    fontSize: '2em',
+    color: '#e1e1e1',
+    marginRight: theme.spacing(1)
+  },
+  userInfo: {
+    display: 'flex',
+    alignItems: 'center',
+    fontWeight: 500
+  },
+  content: {
+    marginTop: theme.spacing(2)
+  }
+}));
