a51a2b8fb4642dc2407c9d08a56a5933c3336f1e
Michael Garganourakis <garganourakis.m@gmail.com>
2019-11-21 23:59:53 +0200

    misc

--- pages/signup/styles.js
+++ pages/signup/styles.js
@@ -1,11 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(theme => ({
-  '@global': {
-    body: {
-      backgroundColor: theme.palette.common.white,
-    },
-  },
   paper: {
     marginTop: theme.spacing(8),
     display: 'flex',
