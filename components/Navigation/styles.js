5ba9aab41bbce3aac35d2f75f513e5549e32cf71
Michael Garganourakis <garganourakis.m@gmail.com>
2019-11-30 22:21:17 +0200

    styles for Navigation

---
+++ components/Navigation/styles.js
@@ -0,0 +1,12 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles(() => ({
+  navigation: {
+    position: 'fixed',
+    bottom: 0,
+    left: 0,
+    width: '100%',
+    zIndex: 1,
+    boxShadow: '0px -1px 7px 1px #d4d4d4',
+  }
+}));
