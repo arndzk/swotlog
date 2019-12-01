b6ed0a2536a7e1365934d699d604c7cb792cd30f
Michael Garganourakis <garganourakis.m@gmail.com>
2019-12-01 15:19:41 +0200

    groups view init

--- pages/groups/index.js
+++ pages/groups/index.js
@@ -1,1 +1,16 @@
-export default () => <div>groups</div>
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+
+import useStyles from './styles';
+
+export default () => {
+  const classes = useStyles();
+
+return <div>
+    groups
+
+  <Fab color="primary" aria-label="add" className={classes.fab}>
+    <AddIcon />
+  </Fab>
+  </div>
+}
